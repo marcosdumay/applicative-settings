0b7b6730b75d3cadec930d738fb18e24ef6cf7ca
Marcos Dumay de Medeiros <marcos@marcosdumay.com>
2017-09-14 19:15:26 -0300

    Mini tutorial at the intoduction of the main module

--- src/ApSettings.hs
+++ src/ApSettings.hs
@@ -1,3 +1,29 @@
+{- |
+#How to use
+
+Define your settings structure and constructor, and create a parser in
+applicative style. Read your settings files and other sources, and
+evaluate the settings into your structure:
+
+    import ApSettings
+
+    data MySettings = MySettings {aSetting :: String, anotherSetting :: [Double], yetAnother :: Int}
+
+    readSettings = do
+        let settingsParser = MySettings <$>
+            onKey "aSetting" `scalar` text <*>
+            onKey "anotherSetting" `multiple` value real <*>
+            onKey "yetAnother" `scalar` integral
+        setts <- mapM readYaml ["file1", "file2", "file3"]
+        pure $ evaluateSettings settingsParser setts
+
+    main = do
+        s' <- readSettings
+        case s' of
+	    Left e -> hPutStr stderr e
+	    Right s -> do some stuff with your (s :: MySettings)
+-}
+
 module ApSettings (
   Key,
   ValueError(..),
